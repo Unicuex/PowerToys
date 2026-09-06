11b3bda3763b3452268e6f21cc6856d5b6ffb05c
Jiří Polášek <me@jiripolasek.com>
2026-09-06 12:27:15 +0200

    Let's make this even better
    To pay for my sins and laziness...

--- src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/SettingsViewModel.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/SettingsViewModel.cs
@@ -15,6 +15,7 @@ using Microsoft.CommandPalette.Extensions.Toolkit;
 namespace Microsoft.CmdPal.UI.ViewModels;
 
 public partial class SettingsViewModel : INotifyPropertyChanged,
+    IDisposable,
     IRecipient<DockAutoHideConflictMessage>
 {
     private static readonly List<TimeSpan> AutoGoHomeIntervals =
@@ -33,6 +34,8 @@ public partial class SettingsViewModel : INotifyPropertyChanged,
     private readonly ISettingsService _settingsService;
     private readonly TopLevelCommandManager _topLevelCommandManager;
     private readonly IMonitorService? _monitorService;
+    private readonly TaskScheduler _uiScheduler;
+    private bool _disposed;
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -159,6 +162,11 @@ public partial class SettingsViewModel : INotifyPropertyChanged,
         get => _settingsService.Settings.CompactMode;
         set
         {
+            if (value == CompactMode)
+            {
+                return;
+            }
+
             _settingsService.UpdateSettings(s => s with { CompactMode = value });
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CompactMode)));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CanConfigureQuickAccessShelf)));
@@ -170,6 +178,11 @@ public partial class SettingsViewModel : INotifyPropertyChanged,
         get => _settingsService.Settings.ShowQuickAccessShelf;
         set
         {
+            if (value == ShowQuickAccessShelf)
+            {
+                return;
+            }
+
             _settingsService.UpdateSettings(s => s with { ShowQuickAccessShelf = value });
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CanConfigureQuickAccessShelf)));
         }
@@ -208,6 +221,11 @@ public partial class SettingsViewModel : INotifyPropertyChanged,
         get => (int)_settingsService.Settings.RecentCommandsOnQuickAccessShelf;
         set
         {
+            if (!Enum.IsDefined((RecentCommandsPlacement)value))
+            {
+                return;
+            }
+
             _settingsService.UpdateSettings(s => s with { RecentCommandsOnQuickAccessShelf = (RecentCommandsPlacement)value });
         }
     }
@@ -217,6 +235,11 @@ public partial class SettingsViewModel : INotifyPropertyChanged,
         get => (int)_settingsService.Settings.RecentCommandsOnHome;
         set
         {
+            if (!Enum.IsDefined((RecentCommandsPlacement)value))
+            {
+                return;
+            }
+
             _settingsService.UpdateSettings(s => s with { RecentCommandsOnHome = (RecentCommandsPlacement)value });
         }
     }
@@ -409,6 +432,7 @@ public partial class SettingsViewModel : INotifyPropertyChanged,
         _settingsService = settingsService;
         _topLevelCommandManager = topLevelCommandManager;
         _monitorService = monitorService;
+        _uiScheduler = scheduler;
 
         Appearance = new AppearanceSettingsViewModel(themeService, settingsService);
         DockAppearance = new DockAppearanceSettingsViewModel(themeService, settingsService);
@@ -472,11 +496,53 @@ public partial class SettingsViewModel : INotifyPropertyChanged,
         }
 
         WeakReferenceMessenger.Default.Register<DockAutoHideConflictMessage>(this);
+        _settingsService.SettingsChanged += SettingsService_SettingsChanged;
     }
 
     public void Receive(DockAutoHideConflictMessage message)
     {
         Dock_AutoHideConflict = message.IsConflict;
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _disposed = true;
+        _settingsService.SettingsChanged -= SettingsService_SettingsChanged;
+        WeakReferenceMessenger.Default.UnregisterAll(this);
+        foreach (var provider in CommandProviders)
+        {
+            provider.Dispose();
+        }
+
+        Appearance?.Dispose();
+        DockAppearance?.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    private void SettingsService_SettingsChanged(ISettingsService sender, SettingsModel settings)
+    {
+        // SettingsChanged may run inside a binding setter or on a background thread.
+        // Queue the refresh so bindings read the latest settings on the UI scheduler.
+        _ = Task.Factory.StartNew(
+            () =>
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CompactMode)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShowQuickAccessShelf)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CanConfigureQuickAccessShelf)));
+            },
+            CancellationToken.None,
+            TaskCreationOptions.None,
+            _uiScheduler);
     }
 
     private IEnumerable<CommandProviderWrapper> GetCommandProviders()
--- src/modules/cmdpal/Microsoft.CmdPal.UI/Settings/GeneralPage.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Settings/GeneralPage.xaml.cs
@@ -18,7 +18,7 @@ using Windows.Win32.UI.Shell;
 
 namespace Microsoft.CmdPal.UI.Settings;
 
-public sealed partial class GeneralPage : Page, INotifyPropertyChanged
+public sealed partial class GeneralPage : Page, INotifyPropertyChanged, IDisposable
 {
     internal const string RecentItemsSettingsElementTag = "RecentItems";
 
@@ -57,7 +57,6 @@ public sealed partial class GeneralPage : Page, INotifyPropertyChanged
         _notificationStateTimer.Tick += NotificationStateTimer_Tick;
 
         Loaded += GeneralPage_Loaded;
-        Unloaded += GeneralPage_Unloaded;
     }
 
     public bool HasExternalCommandPermissions
@@ -134,12 +133,15 @@ public sealed partial class GeneralPage : Page, INotifyPropertyChanged
         NavigateToPendingSettingsElement();
     }
 
-    private void GeneralPage_Unloaded(object sender, RoutedEventArgs e)
+    public void Dispose()
     {
         _isPageLoaded = false;
+        Loaded -= GeneralPage_Loaded;
         _notificationStateTimer.Stop();
+        _notificationStateTimer.Tick -= NotificationStateTimer_Tick;
         _settingsService.SettingsChanged -= SettingsService_SettingsChanged;
         _externalCommandPermissionStore.PermissionsChanged -= ExternalCommandPermissionStore_PermissionsChanged;
+        viewModel?.Dispose();
     }
 
     private void ExternalCommandPermissionStore_PermissionsChanged(object? sender, EventArgs e) =>
